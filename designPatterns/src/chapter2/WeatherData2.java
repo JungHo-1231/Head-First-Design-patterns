b50b41db1d32318d784b0219e8c8a8299f277e99
JungHo <jungpark007@naver.com>
2020-11-12 01:36:04 +0900

    chapter 2 옵저버 패턴

---
+++ designPatterns/src/chapter2/WeatherData2.java
@@ -0,0 +1,36 @@
+package chapter2;
+
+import java.util.Observable;
+
+// 자바 내장 기능을 활용함.
+public class WeatherData2 extends Observable {
+   private float temperature;
+   private float humidity;
+   private float pressure;
+
+   public WeatherData2() {
+   }
+
+   public void measurementsChanged(){
+      setChanged();
+      notifyObservers();
+   }
+
+  public void setMeasurements(float temperature, float humidity, float pressure) {
+      this.temperature = temperature;
+      this.humidity = humidity;
+      this.pressure = pressure;
+  }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+}
---
+++ designPatterns/src/chapter2/WeatherStation.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData2 weatherData2 = new WeatherData2();
+
+        CurrentConditionsDisplay2 currentConditionsDisplay2 = new CurrentConditionsDisplay2(weatherData2);
+
+        weatherData2.setMeasurements(80, 65, 30.4F);
+        weatherData2.setMeasurements(82, 70, 29.2F);
+        weatherData2.setMeasurements(78, 90, 29.2F);
+    }
+}
